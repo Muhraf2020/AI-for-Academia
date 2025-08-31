9d113c6b004fbd58b3f6688471e4c538ced3196c
Muhraf2020 <143078152+Muhraf2020@users.noreply.github.com>
2025-08-31 18:17:35 +0200

    app.js

--- assets/app.js
+++ assets/app.js
@@ -4,7 +4,7 @@
   const CONFIG = {
     GOOGLE_FORM_URL: "https://docs.google.com/forms/d/e/1FAIpQLScUEGeF_jxL3oNwCTrBh1OX-WRlsauQksUJjV_Z3uiY9k9pYA/viewform", // <-- replace
     ITEMS_PER_PAGE: 12,
-    SITE_URL: "https://yourname.github.io/ai-tools-directory/", // <-- replace when you publish
+    SITE_URL: "https://academiawithai.com/", // <-- replace when you publish
     ASSET_VERSION: "1" // bump to "2", "3", ... whenever you update logos
   };
 
